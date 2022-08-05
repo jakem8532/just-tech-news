37d048749244abef3b4ac6ec5d62283a4aa10f41
jakem8532 <85272755+jakem8532@users.noreply.github.com>
2022-08-04 22:40:10 -0400

    added single-post page, and cleaned up page

---
+++ public/javascript/upvote.js
@@ -0,0 +1,26 @@
+const upvoteBtn = document.querySelector('.upvote-btn')
+
+async function upvoteClickHandler(event) {
+    event.preventDefault()
+
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ]
+
+    const res = await fetch('/api/posts/upvote', {
+        method: 'put',
+        body: JSON.stringify({
+            post_id: id
+        }),
+        headers: { 'Content-Type': 'application/json'}
+    })
+
+    if (res.ok) {
+        document.location.reload()
+    }else {
+        alert(res.statusText)
+    }
+
+}
+
+upvoteBtn.addEventListener('click', upvoteClickHandler)
